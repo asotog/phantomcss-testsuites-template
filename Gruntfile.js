70416dce40508f189c4bcfef9edd1e6c6b553c52
asotog <asotog88@yahoo.es>
2014-03-04 21:39:12 -0600

    phantomcss config file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,7 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        phantomcss: {
-            desktop: {
-                options: {
-                    screenshots: 'tests-outputs/desktop/screenshots/',
-                    results: 'tests-outputs/desktop/results/'
-                },
-                src: [
-                    'testsuites/*.js'
-                ]
-            },
-            phone: {
-                options: {
-                    screenshots: 'tests-outputs/phone/screenshots/',
-                    results: 'tests-outputs/phone/results/',
-                    viewportSize: [320, 480]
-                },
-                src: [
-                    'testsuites/*.js'
-                ]
-            }
-        }
+        phantomcss: grunt.file.readJSON('phantomcss.json')
     });
 
     grunt.loadNpmTasks('grunt-phantomcss');
